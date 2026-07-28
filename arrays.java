721dbd49c238b24868bcdebec1db385b53153811
Hrishikesh Kamble <hrishikeshyk772005@gmail.com>
2026-07-28 10:49:05 +0530

    first commit

---
+++ arrays.java
@@ -0,0 +1,31 @@
+public class arrays{
+   public static int linearsearch(int numbers[],int key){
+   for(int i=0;i<numbers.length;i++){
+    if(numbers[i]==key){
+        return i;
+    }
+   }
+   return -1;
+}
+   
+   
+   
+   
+   
+   
+
+
+
+
+    public static void main(String[] args) {
+        int numbers[]={2,4,3,6,8,12};
+        int key=10;
+        int index=linearsearch(numbers,key);
+        if(index==-1){
+            System.out.println("not found");
+        }
+        else{
+            System.out.println("found at index"+index);
+        }
+    }
+}
