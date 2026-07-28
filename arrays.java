b6a8aed5c4bfb227749417b4e55adb68b23c2574
Hrishikesh Kamble <hrishikeshyk772005@gmail.com>
2026-07-28 11:34:06 +0530

    LArgestInAnArray

--- arrays.java
+++ arrays.java
@@ -1,31 +1,22 @@
+import java.util.*;
 public class arrays{
-   public static int linearsearch(int numbers[],int key){
-   for(int i=0;i<numbers.length;i++){
-    if(numbers[i]==key){
-        return i;
-    }
-   }
-   return -1;
-}
-   
-   
-   
-   
-   
-   
+    public static int getlargest(int numbers[]){
+        int largest=Integer.MIN_VALUE;
+            for(int i=0;i<numbers.length;i++){
+                if(largest<numbers[i]){
+                    largest=numbers[i];
+                }
+            }
 
-
-
-
-    public static void main(String[] args) {
-        int numbers[]={2,4,3,6,8,12};
-        int key=10;
-        int index=linearsearch(numbers,key);
-        if(index==-1){
-            System.out.println("not found");
+            return largest;
         }
-        else{
-            System.out.println("found at index"+index);
-        }
+
+    
+    
+    public static void main(String args[]){
+        int numbers[]={1,2,4,10,12,18};
+        int largest=getlargest(numbers);
+        System.out.println("largest value is:"+largest);
     }
 }
+    
